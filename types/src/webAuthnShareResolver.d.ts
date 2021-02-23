e92333a8dcb1318d4c4e0fb3b748cf44d35d909d
Leonard Tan <tetratorus@gmail.com>
2021-02-23 16:18:59 +0800

    update types

--- types/src/webAuthnShareResolver.d.ts
+++ types/src/webAuthnShareResolver.d.ts
@@ -1,5 +1,5 @@
 import { Ecies } from "@toruslabs/eccrypto";
-import MetadataStorageLayer from "./MetadataStorageLayer";
+import MetadataStorageLayer, { PubKeyParams } from "./MetadataStorageLayer";
 declare type EciesHex = {
     iv: string;
     ephemPublicKey: string;
@@ -8,8 +8,8 @@ declare type EciesHex = {
 };
 export declare function encParamsHexToBuf(encParamsHex: EciesHex): Ecies;
 export declare function encParamsBufToHex(encParams: Ecies): EciesHex;
-export declare function setTorusShare(m: MetadataStorageLayer, webAuthnKeyHex: string, subspace: string, subspaceData: unknown): Promise<void>;
+export declare function setTorusShare(m: MetadataStorageLayer, webAuthnPubKey: PubKeyParams, webAuthnRefHex: string, subspace: string, subspaceData: unknown): Promise<void>;
 export declare function setDeviceShare(m: MetadataStorageLayer, webAuthnRefHex: string, subspace: string, subspaceData: unknown): Promise<void>;
-export declare function getTorusShare<T>(m: MetadataStorageLayer, webAuthnKeyHex: string, subspace: string): Promise<T | null>;
+export declare function getTorusShare<T>(m: MetadataStorageLayer, webAuthnKeyHex: string, webAuthnRefHex: string, subspace: string): Promise<T | null>;
 export declare function getDeviceShare<T>(m: MetadataStorageLayer, webAuthnRefHex: string, subspace: string): Promise<T | null>;
 export {};
