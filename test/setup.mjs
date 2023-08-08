157a10a0020ae07fd297b6d380fc25badcbe5452
Archit <arch1995@gmail.com>
2023-08-08 13:58:37 +0530

    remove atob & bota

--- test/setup.mjs
+++ test/setup.mjs
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import register from "@babel/register";
-import atob from "atob";
-import btoa from "btoa";
 import path from "path";
 import { register as tsRegister } from "ts-node";
 
@@ -10,6 +8,3 @@ tsRegister({ project: path.resolve("tsconfig.json") });
 register({
   extensions: [".ts", ".js"],
 });
-
-global.atob = atob;
-global.btoa = btoa;
