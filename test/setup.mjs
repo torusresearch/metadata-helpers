0773df91a252a5ef90e863e0749616e9a56e55aa
Archit <arch1995@gmail.com>
2023-08-08 12:54:56 +0530

    Remove eslint plugins

--- test/setup.mjs
+++ test/setup.mjs
@@ -1,7 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import register from "@babel/register";
 import atob from "atob";
 import btoa from "btoa";
-import fetch from "node-fetch";
 import path from "path";
 import { register as tsRegister } from "ts-node";
 
@@ -11,6 +11,5 @@ register({
   extensions: [".ts", ".js"],
 });
 
-global.fetch = fetch;
 global.atob = atob;
 global.btoa = btoa;
